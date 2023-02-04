693085f75af03cb68456badb18864677f3f4ecf1
rgibbons131 <richiegibbons99@gmail.com>
2023-02-04 16:48:04 -0700

    helpful comments

--- src/Currency.java
+++ src/Currency.java
@@ -1,11 +1,14 @@
 
 
 public class Currency {
+
+    // each is a ratio to the us dollar
     float usd = 1;
     float eur =  0.91352f;
     float cad = 1.33074f;
     float mxn = 18.6073f;
 
+    //String input to return one of the ratios. If it is a not valid code it will return 1 and the currency will just print as input
     public float getRate(String type){
         if (type.equals("usd")){
             return usd;
